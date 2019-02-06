c150374cc8e4e1d26a31e4f0665656366b83cc52
kelleyje <kelleyje@rose-hulman.edu>
2019-02-06 12:47:01 -0500

    Includes testing code for raising the arm and claw. Successful test.

--- src/m3_run_this_on_robot.py
+++ src/m3_run_this_on_robot.py
@@ -17,6 +17,13 @@ def main():
       2. Communicates via MQTT with the GUI code that runs on the LAPTOP.
     """
 
+    run_test_raise_arm()
+
+
+def run_test_raise_arm():
+    robot = rosebot.RoseBot()
+    robot.arm_and_claw.raise_arm()
+
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
