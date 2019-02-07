7c2229977294bd1e7872f7f1e6b680d93207b62e
mossac <mossac@rose-hulman.edu>
2019-02-07 12:04:47 -0500

    m1 work

--- src/m1_run_this_on_robot.py
+++ src/m1_run_this_on_robot.py
@@ -16,8 +16,16 @@ def main():
       1. Makes the EV3 robot to various things.
       2. Communicates via MQTT with the GUI code that runs on the LAPTOP.
     """
+    real_thing()
 
+def real_thing():
+    robot = rosebot.RoseBot()
+    delegate_recieves = DelegateRecieves(robot)
+    mqtt_reciever = com.MqttClient(delegate_recieves)
+    mqtt_reciever.connect_to_pc()
 
+    while True:
+        time.sleep(.01)
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
 # -----------------------------------------------------------------------------
