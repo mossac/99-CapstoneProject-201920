14e7ccb3fb791651cbd2cc6a73325de1f4a008ef
yangg <yangg@rose-hulman.edu>
2019-02-11 12:20:13 -0500

    move_to_position

--- src/m2_run_this_on_laptop.py
+++ src/m2_run_this_on_laptop.py
@@ -23,44 +23,59 @@ def main():
     # Construct and connect the MQTT Client:
     # -------------------------------------------------------------------------
 
-
+    mqtt_sender=com.MqttClient()
+    mqtt_sender.connect_to_ev3()
     # -------------------------------------------------------------------------
     # The root TK object for the GUI:
     # -------------------------------------------------------------------------
-
+    root = tkinter.Tk()
+    root.title("Capstone Project")
 
     # -------------------------------------------------------------------------
     # The main frame, upon which the other frames are placed.
     # -------------------------------------------------------------------------
+    main_frame = ttk.Frame(root, padding=10, borderwidth=5, relief="groove")
+    main_frame.grid()
 
 
     # -------------------------------------------------------------------------
     # Sub-frames for the shared GUI that the team developed:
     # -------------------------------------------------------------------------
+    teleop_frame, arm_frame, control_frame, drive_frame, sound_frame = get_shared_frames(main_frame, mqtt_sender)
 
 
     # -------------------------------------------------------------------------
     # Frames that are particular to my individual contributions to the project.
     # -------------------------------------------------------------------------
-    # TODO: Implement and call get_my_frames(...)
+    # DONE: Implement and call get_my_frames(...)
 
     # -------------------------------------------------------------------------
     # Grid the frames.
     # -------------------------------------------------------------------------
 
-
+    grid_frames(teleop_frame, arm_frame, control_frame, drive_frame, sound_frame)
     # -------------------------------------------------------------------------
     # The event loop:
     # -------------------------------------------------------------------------
-
+    root.mainloop()
 
 
 def get_shared_frames(main_frame, mqtt_sender):
-    pass
+    teleop_frame = shared_gui.get_teleoperation_frame(main_frame, mqtt_sender)
+    arm_frame = shared_gui.get_arm_frame(main_frame, mqtt_sender)
+    control_frame = shared_gui.get_control_frame(main_frame, mqtt_sender)
+    drive_frame = shared_gui.get_drive_frame(main_frame, mqtt_sender)
+    sound_frame = shared_gui.get_sound_system_frame(main_frame, mqtt_sender)
+
+    return teleop_frame, arm_frame, control_frame, drive_frame, sound_frame
 
 
 def grid_frames(teleop_frame, arm_frame, control_frame):
-    pass
+    teleop_frame.grid(row=0, column=0)
+    arm_frame.grid(row=1, column=0)
+    control_frame.grid(row=2, column=0)
+    drive_frame.grid(row=3, column=0)
+    sound_frame.grid(row=4, column=0)
 
 
 # -----------------------------------------------------------------------------
